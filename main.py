70648614ca5cf62854b20825c165d9a7c2460b28
Roman <graff049@gmail.com>
2022-07-17 17:46:41 +0300

    feature(yandex_pay_token): add example_yandex_pay_token

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+"""The main module for running CloudPayments client"""
 import aiohttp
 from environs import Env
-
+import uuid
 from cloudpayments_client import CloudPaymentsClient
 
 
@@ -11,18 +12,16 @@ def run_cloudpayments_client():
     public_id = env.str('PUBLIC_ID')
     api_secret = env.str('API_SECRET')
 
-    payment_data_cryptogram = '014242424242250102X6424dZ5oVeTMY5j5sA4Z1LrRz9uvjS4W7FJghHVbLSGta4GDB4lvyP0YG7HhcWQDe' \
-                              'DAxoSQG9HxBbmCObxGjb0PYGy0r22/+gPRCOnpSYkaCI/tMr3TnCUA1PrcSLIHBUTk5AIOyzW4TT2L+cP1Fx' \
-                              'nOaJ2Hpy3LpjjMYVOKt0pPthT1ol/2esAdknuYbh3Pz1Ga5Fo86KQ18WsFvMbM+5XSKU5r5eiDmU7njK8RI5' \
-                              'Sxvput8PgRYOdFUl1uORPyKhbqz/obJyhlpy8/wS2qZiyyXp0zNmHzGLb/LYZVZhs1Ay1f3iKrRRc3uK6t4x' \
-                              '5s7zYn0mAm0DsEr/iNh7AXuQ=='
-
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    example_yandex_pay_token = "\"{\\\"type\\\":\\\"Yandex\\\",\\\"signedMessage\\\":\\\"{\\\\\\\"encryptedMessage\\\\\\\":\\\\\\\"xqpAiS2L71BZNgH514AQDwOVawJF4gHXF8P+ECIFRqFHlDMRtxHsO9hNQSeegSssRdDMlBIyOObY5dqI3iwX99UKYP6qFD+tKEYJQkUdiKyhZCwgUsVdHBlFQA+iiXVLf7DZ5WCIaHjpl4mckrGeDg4XGDIX4FB0BorLqocbDLcl0JZi2zzkNtn9FDLPSAs1qbTEMdb3TAS0iDAIkuAy5DGJ3+4Av9PWvIllW4LRdQ34rR8MPszJxq9Xagw/jeKUglyUERQgi5cnVWIB992yPh9UFgNuCQBc+JWLMzuOIKKxFiVK6VBSsuHpDWrSZqMolN6PIeNvETxQ34g+O/u4KiwWd3IG/pb5e0FYbzn/gWzlDSPsqNSuB713qZDHCI7eFB7h7iPTdk/Wd78Vv7Vlg4oVQdMWCbgSjtWDamKeq/OMiVDW5j36CebRQWxB8/XFj4nAInHIjoUUKsEQ5gf00n9/48RUNVCbRr6qykvsfnD0XP5V4OJOeIhAZN2CAgGxgrGC5MibfjAf+D/EnunHwOvtmI6KQAsGv9QgrRC8sxTeyk7OT9vUCzK2DIRDYyCtvloGalRq1PRdJWQX\\\\\\\",\\\\\\\"tag\\\\\\\":\\\\\\\"LTx6/HA9iWaZwbYaFN1j9aDOPp2PBlR2iBMUBQ7zyUg=\\\\\\\",\\\\\\\"ephemeralPublicKey\\\\\\\":\\\\\\\"BHHBcT4SvFgxMK14Oz3/dk/uiCL2m4jeTFDEcoYHXt5gAz2wFVEnvRD4fHArkbIOcry9nlUYHWgT4GicEl9qkXY=\\\\\\\"}\\\",\\\"protocolVersion\\\":\\\"ECv2\\\",\\\"signature\\\":\\\"MEUCICyyzWnCEf2iHlUszDzvbAx/qk/sLmbTaOWPVEq1hr29AiEA0lfZ85pCofYhxVX971Xtshysawi7+KEe8ZpPVlV/Md4=\\\",\\\"intermediateSigningKey\\\":{\\\"signedKey\\\":\\\"{\\\\\\\"keyValue\\\\\\\":\\\\\\\"MFkwEwYHKoZIzj0CAQYIKoZIzj0DAQcDQgAEqYNePt6BPgCv5JxfO9dF2vrSqmnp4Mhe/vF+XO+Devbs6/KVpVVoTD8LLcAo4TZh6IuODVnVpHrTObhg3HJVJA==\\\\\\\",\\\\\\\"keyExpiration\\\\\\\":\\\\\\\"1764950892000\\\\\\\"}\\\",\\\"signatures\\\":[\\\"MEQCIDRslMW7wNZbpqVw/dD7hDQh30hGhqfjfWTBvc7zAYJSAiAGAvjAslA2AxwdAEuOfacFr6DaE5yiiUuUtM6DUreZYg==\\\"]}}\""
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+        'X-Request-ID': str(uuid.uuid4()),
+    }
     auth = aiohttp.BasicAuth(login=public_id, password=api_secret, encoding='utf-8')
     params = {
         'Amount': 100,
         'IpAddress': '123.123.123.123',
-        'CardCryptogramPacket': payment_data_cryptogram,
+        'CardCryptogramPacket': example_yandex_pay_token,
     }
 
     cloudpayments_client = CloudPaymentsClient()
