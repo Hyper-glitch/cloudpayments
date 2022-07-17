f86727c6a35540120deca8b61b5ea38067874786
Roman <graff049@gmail.com>
2022-07-18 00:17:48 +0300

    refactoring

--- marshmallow_schemas.py
+++ marshmallow_schemas.py
@@ -1,24 +1,8 @@
 """Module for marshmallow schemas."""
-from marshmallow import Schema, fields, validates
-
-from exceptions import TransactionValueError
+from marshmallow import Schema, fields
 
 
 class ParamsSchema(Schema):
-    Amount = fields.Integer()
-    IpAddress = fields.String()
-    CardCryptogramPacket = fields.String()
-
-    @validates('amount')
-    def validate_amount(self, amount: int) -> None:
-        """Validate amount of transaction, if it less than 0.01, then raise an exception.
-        :param amount: Payment amount, Numeric, Required.
-        :return: None
-        """
-        min_transaction_value = 0.01
-        if amount < min_transaction_value:
-            raise TransactionValueError(
-                service=None,
-                method=None,
-                message='The amount parameter does not accept a transaction amount less than 0.01.',
-            )
+    Amount = fields.Integer(required=True)
+    IpAddress = fields.String(required=True)
+    CardCryptogramPacket = fields.String(required=True)
