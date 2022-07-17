74fec02fcc058ae1ae4c1aee07dde4ff6fbd721a
Roman <graff049@gmail.com>
2022-07-18 00:53:04 +0300

    refactor(doc): add docstrings

--- marshmallow_schemas.py
+++ marshmallow_schemas.py
@@ -3,12 +3,14 @@ from marshmallow import Schema, fields, validate
 
 
 class ParamsSchema(Schema):
+    """Schema for serializing params."""
     Amount = fields.Integer(required=True)
     IpAddress = fields.String(required=True)
     CardCryptogramPacket = fields.String(required=True)
 
 
 class SuccessfulResponseSchema(Schema):
+    """Schema for deserializing and validate is response successful."""
     Model = fields.Dict()
     Success = fields.Boolean(validate=validate.Equal(True))
     Message = fields.String(missing=None)
