2d48694fbcbb51f611f3d33bc100148d7d2a4dbc
Roman <graff049@gmail.com>
2022-07-18 00:51:44 +0300

    feature(schema): add SuccessfulResponseSchema

--- marshmallow_schemas.py
+++ marshmallow_schemas.py
@@ -1,8 +1,14 @@
 """Module for marshmallow schemas."""
-from marshmallow import Schema, fields
+from marshmallow import Schema, fields, validate
 
 
 class ParamsSchema(Schema):
     Amount = fields.Integer(required=True)
     IpAddress = fields.String(required=True)
     CardCryptogramPacket = fields.String(required=True)
+
+
+class SuccessfulResponseSchema(Schema):
+    Model = fields.Dict()
+    Success = fields.Boolean(validate=validate.Equal(True))
+    Message = fields.String(missing=None)
