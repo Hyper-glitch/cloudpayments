0bcb4e3ffbed7127d43c773842a9d233c6391f34
Roman <graff049@gmail.com>
2022-07-17 16:34:07 +0300

    feature(cloudpayments): continue coding

--- cloudpayments_client.py
+++ cloudpayments_client.py
@@ -1,29 +1,34 @@
 """Module for Cloudpayments API client."""
 import urllib.parse as urllib
 
-from abstract_client import AbstractInteractionClient
+from aiohttp import BasicAuth
+
+from abstract_client import AbstractInteractionClient, BaseInteractionError
 
 
-class TransactionValueError(Exception):
-    def __init__(self):
-        self.message = 'The amount parameter does not accept a transaction amount less than 0.01.'
+class TransactionValueError(BaseInteractionError):
+    pass
 
 
 class CloudPaymentsClient(AbstractInteractionClient):
     """Cloudpayments API client, inherited from AbstractInteractionClient."""
     BASE_URL = 'https://api.cloudpayments.ru/'
+    SERVICE = 'CloudPayments'
 
     def __int__(self):
         self.test_url = urllib.urljoin(self.BASE_URL, 'test')
         self.cloudpayments_session = self.create_session()
 
-    @staticmethod
-    def validate_amount(amount):
+    def validate_amount(self, amount):
         min_transaction_value = 0.01
         if amount < min_transaction_value:
-            raise TransactionValueError
+            raise TransactionValueError(
+                service=self.SERVICE,
+                method=None,
+                message='The amount parameter does not accept a transaction amount less than 0.01.',
+            )
 
-    def charge(self, params: dict, one_stage_payment: bool = None):
+    def charge(self, auth: BasicAuth, headers: dict, params: dict, one_stage_payment: bool = None):
         """
         Method for payment by payment data cryptogram result of encryption algorithm.
         :param one_stage_payment: flag, that define which payment we need to use.
@@ -38,13 +43,7 @@ class CloudPaymentsClient(AbstractInteractionClient):
             url = self.endpoint_url(relative_url=two_stage_endpoint)
 
         self.validate_amount(params['Amount'])
-        self.post(interaction_method='', url=url, **params) # maybe we should use asyncio
+        response = await self.post(interaction_method='', url=url, **params)  # maybe we should use asyncio
 
-
-if __name__ == '__main__':
-    params = {
-        'Amount': 100,
-
-    }
-    cloudpayments_client = CloudPaymentsClient()
-    cloudpayments_client.charge(params=params, one_stage_payment=True)
+        if response['Success']:
+            token = ''
